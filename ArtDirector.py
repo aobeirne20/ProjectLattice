9e4f59405463cc455218fc7b51e367cab1124e19
aobeirne20 <56138845+aobeirne20@users.noreply.github.com>
2021-09-19 19:53:21 -0600

    branch basic setup, and JSON metadata saving

---
+++ ArtDirector.py
@@ -0,0 +1,62 @@
+import ArtAssistant as AA
+import ImageGen as IG
+
+import os
+import json
+
+
+class ArtDirector:
+    def __init__(self):
+        self.art_order = []
+        self.batch_value = None
+
+    def calc_order_dist(self, art_order, art_types_n, art_types_p):
+        print(f"ORDER SUMMARY:")
+        for city in art_order.keys():
+            self.art_order.append({"City": city,
+                                   "Order": AA.dist_calculator(art_order[city], art_types_n, art_types_p)})
+            self.art_order[-1]["RList"] = AA.order_randomizer(self.art_order[-1]['Order'])
+            sorted_order = {k: v for k, v in sorted(self.art_order[-1]["Order"].items(), key=lambda item: item[1])}
+            print(f"{city}: {sorted_order}")
+        print(f"")
+
+    def create_gallery_structure(self):
+        batch_value_txt = open("batch_value", 'r')
+        self.batch_value = int(batch_value_txt.readline())
+        batch_value_txt.close()
+        batch_value_txt = open("batch_value", 'w')
+        batch_value_txt.write(str(self.batch_value + 1))
+        batch_value_txt.close()
+
+        os.makedirs(f"Gallery/Batch_{self.batch_value:03d}/All")
+        for city in self.art_order:
+            os.makedirs(f"Gallery/Batch_{self.batch_value:03d}/{city['City']}/All")
+            for art_type in city["Order"].keys():
+                os.makedirs(f"Gallery/Batch_{self.batch_value:03d}/{city['City']}/{art_type}")
+
+    def i_am_a_creative_type(self):
+        for city in self.art_order:
+            print(f"Starting generation for {city['City']}:")
+            for art_type, num in enumerate(city["RList"]):
+                ig = IG.ImageGen(city=city, art_type=art_type)
+                image, metadata = ig.ex_nihilo_res()
+                image.save(f"Gallery/Batch_{self.batch_value:03d}/All/RapidTopology_{num:05d}", format=".png")
+                image.save(f"Gallery/Batch_{self.batch_value:03d}/{city['City']}/All/RapidTopology_{num:05d}", format=".png")
+                image.save(f"Gallery/Batch_{self.batch_value:03d}/{city['City']}/{art_type}RapidTopology_{num:05d}", format=".png")
+
+                with open(f"Gallery/Batch_{self.batch_value:03d}/All/RapidTopology_{num:05d}.json", 'w') as outfile:
+                    json.dump(metadata, outfile)
+                with open(f"Gallery/Batch_{self.batch_value:03d}/{city['City']}/All/RapidTopology_{num:05d}.json", 'w') as outfile:
+                    json.dump(metadata, outfile)
+                with open(f"Gallery/Batch_{self.batch_value:03d}/{city['City']}/{art_type}RapidTopology_{num:05d}.json", 'w') as outfile:
+                    json.dump(metadata, outfile)
+
+    def i_sell_hotdogs(self, cities, art_type):
+        for city, num in enumerate(cities):
+            print(f"Starting probability-blind generation for {city}:")
+            ig = IG.ImageGen(city=city, art_type=art_type)
+            image, metadata = ig.ex_nihilo_res()
+            image.save(f"RapidTopology_{num:05d}", format=".png")
+            with open(f"RapidTopology_{num:05d}.json", 'w') as outfile:
+                json.dump(metadata, outfile)
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import ArtDirector as AD
+
+director = AD.ArtDirector()
+# director.calc_order_dist(art_order={"London": 1863, "Paris": 1900, "New York": 1904,
+#                                     "Moscow": 1935, "Tokyo": 1927, "Beijing": 1971},
+#                          art_types_n={"Signature": 1, "Gold": 10, "Gold_Invert": 5},
+#                          art_types_p={"Color": -1, "Color_Invert": 0.10, "Dark": 0.10, "Dark_Invert": 0.01,
+#                                       "Brightline": 0.10, "Brightline_Invert": 0.01,
+#                                        "Grey": 0.20, "Grey_Invert": 0.02})
+# director.create_gallery_structure()
+director.i_sell_hotdogs(cities=["London"], art_type="Color")
+
+
+
+
+
+
+
+
+
+
+
